cd8daeb926a78148d5175b52d6e3eb839c5604b8
MoShabayk <alshabayk123@gmail.com>
2023-05-20 19:33:21 +0200

    Modified the empty route

--- server/routes/emptyRoutes.js
+++ server/routes/emptyRoutes.js
@@ -1,8 +1,9 @@
-const express = require("express");
+import express from"express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "working fine", status: 200 });
-});
+    res.send("Welcome to the server");
+})
 
-module.exports = router;
+export default router;
+
